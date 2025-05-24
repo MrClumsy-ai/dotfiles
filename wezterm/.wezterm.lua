e25836000d7a2d81e34c76eaca167da0e3aef64d
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-24 13:29:15 -0600

    initial commit

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+config.color_scheme = "Gruvbox dark, hard (base16)"
+config.colors = { background = "black" }
+config.font_size = 16
+config.window_background_opacity = 0.6
+config.enable_tab_bar = false
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_ease_out = "Linear"
+config.cursor_blink_rate = 400
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
+config.font = wezterm.font("JetBrains Mono", {})
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+return config
