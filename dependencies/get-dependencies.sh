c7919ac876c5016f5d5d0defb4bfef47f8af27c3
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-24 19:13:04 -0600

    new(get-dependencies): bash script to install all dependencies

---
+++ dependencies/get-dependencies.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "hello epic gamer"
+echo "updating your epic gaming pc"
+sudo apt update && sudo apt upgrade -y && \
+sudo apt autoremove && sudo apt-get update -y
+echo "installing allat"
+sudo apt-get install ninja-build gettext cmake \
+curl build-essential nodejs npm golang git pip \
+libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python3-openssl \
+binutils bison gcc ripgrep zsh neofetch unzip fzf tmux \
+flatpak gnome-software-plugin-flatpak copyq i3 feh dmenu \
+picom xinput polybar brightnessctl stow
+echo "ig this is it gng..."
