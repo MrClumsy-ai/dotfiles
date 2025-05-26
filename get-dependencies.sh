d8e64fac814bc6f69c04090be97c2301d321ea68
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-26 16:45:17 -0600

    mod(get-dependencies): added steam-devices

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,7 +17,8 @@ xz-utils tk-dev libffi-dev liblzma-dev python3-openssl \
 binutils bison gcc ripgrep zsh neofetch unzip fzf tmux \
 flatpak gnome-software-plugin-flatpak copyq i3 feh dmenu \
 picom xinput polybar brightnessctl stow python3-venv zip \
-gimp wine wine32 wine64 libwine libwine:i386 fonts-wine
+gimp wine wine32 wine64 libwine libwine:i386 fonts-wine \
+steam-devices
 
 echo "probably a good idea to reboot vro"
 echo "ig this is it gng..."
