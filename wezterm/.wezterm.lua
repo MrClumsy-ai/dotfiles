73be4b5228e5f8447a4aeff693af132ae182f123
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-25 22:03:54 -0600

    mod(wezterm): added ctrl+backspace for deleting word

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+config.warn_about_missing_glyphs = false
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.colors = { background = "black" }
 config.font_size = 16
@@ -14,6 +15,10 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
+config.keys({
+	-- ctrl + backspace = delete word backwards
+	{ key = "Backspace", mods = "CTRL", action = wezterm.action.SendString("\x17") },
+})
 config.font = wezterm.font("JetBrains Mono", {})
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
