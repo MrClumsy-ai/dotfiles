dd53b9808fc095bc7f04adf4772de0735f2c5035
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-31 01:46:04 -0600

    new(setup-nvim): bash script to build neovim

---
+++ setup-nvim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "installing all building requirements"
+sudo apt-get install ninja-build gettext cmake curl build-essential
+echo "cloning into neovim repository"
+git clone https://github.com/neovim/neovim ~/git/neovim
+cd ~/git/neovim
+echo "building neovim"
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+echo "installing neovim"
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux-x86_64.deb
+cd ~/git
+echo "removing neovim repository"
+rm -rf neovim
