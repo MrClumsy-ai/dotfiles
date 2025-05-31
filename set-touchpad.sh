8d5d068f0b5fd9e099568b2696ba88e1e1211f08
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-05-31 02:09:21 -0600

    new(setup-i3): bash script to install i3 and all dependencies

--- set-touchpad.sh
+++ set-touchpad.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 xinput set-prop 11 299 1
 xinput set-prop 11 320 1
---
+++ setup-i3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install i3 feh dmenu picom xinput polybar \
+  brightnessctl flameshot 
