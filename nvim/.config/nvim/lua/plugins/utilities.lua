1fc988eaddae3cb8cb12e1bd272e765dddf91c4a
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-04 14:45:24 -0600

    mod(nvim): git pull --rebase with leader gP

--- nvim/.config/nvim/lua/plugins/utilities.lua
+++ nvim/.config/nvim/lua/plugins/utilities.lua
@@ -60,6 +60,7 @@ return {
 		config = function()
 			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 			vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+			vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>")
 		end,
 	},
 	{
