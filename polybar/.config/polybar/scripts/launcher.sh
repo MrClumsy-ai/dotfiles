cbc47acdec963d6b7c47be43ace8681719864bf3
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-05 19:57:04 -0600

    mod(launcher): mod+m for drun, has same style as combi

--- polybar/.config/polybar/scripts/launcher.sh
+++ polybar/.config/polybar/scripts/launcher.sh
@@ -2,4 +2,4 @@
 FILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
 sed -i -e "s/ac: .*/ac:   #FFAF00FF;/g" $FILE
 sed -i -e "s/se: .*/se:   #FFAF0040;/g" $FILE
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/scripts/rofi/launcher.rasi
+rofi -no-lazy-grab -show drun -modi drun
