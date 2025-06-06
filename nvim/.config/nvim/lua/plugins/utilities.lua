c7dcbdab67bab96f323b62250f150dae3bfa45b4
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-05 21:31:34 -0600

    mod(nvim): added cloak for .env

--- nvim/.config/nvim/lua/plugins/utilities.lua
+++ nvim/.config/nvim/lua/plugins/utilities.lua
@@ -75,4 +75,25 @@ return {
 			},
 		},
 	},
+	{
+		"laytan/cloak.nvim",
+		config = function()
+			require("cloak").setup({
+				enable = true,
+				cloak_character = "*",
+				highlight_group = "Comment",
+				cloak_length = nil,
+				try_all_patterns = true,
+				cloak_telescope = true,
+				cloak_on_leave = false,
+				patterns = {
+					{
+						file_pattern = ".env*",
+						cloak_pattern = "=.+",
+						replace = nil,
+					},
+				},
+			})
+		end,
+	},
 }
