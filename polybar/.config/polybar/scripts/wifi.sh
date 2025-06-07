ba85fbe28e125cfaba75945856ddc383d977247e
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-07 10:54:35 -0600

    mod(nvim/navigation): dvorak harpoon

--- polybar/.config/polybar/scripts/wifi.sh
+++ polybar/.config/polybar/scripts/wifi.sh
@@ -2,12 +2,12 @@
 
 dir="~/.config/polybar/scripts/rofi"
 wifi_list=$(nmcli -t -f SSID,BARS dev wifi)
-rofi_command="rofi -no-config"
+rofi_command="rofi"
 chosen="$(echo -e "$wifi_list" | $rofi_command -p "Select Wi-Fi" -dmenu -selected-row 0)"
 ssid=$(echo "$chosen" | cut -d':' -f1)
 
 if [ -n "$ssid" ]; then
-    password=$(rofi -dmenu -p "Password for $ssid" -password)
+    password=$($rofi_command -dmenu -p "Password for $ssid")
     if [ -n "$password" ]; then
         nmcli dev wifi connect "$ssid" password "$password" | \
             notify-send "Wi-Fi" "Connecting to $ssid..."
