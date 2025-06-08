dd239cda4e8af66c61ed5d6e537cd68ecbc6e360
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-08 13:01:21 -0600

    mod(nvim/formatting): added more formatters

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -4,22 +4,19 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "autopep8" },
-				go = { "gofumpt" },
-				c = { "clang-format" },
-				bash = { "beautysh" },
-				cs = { "clang-format" },
-        rust = {"rustfmt"},
+        bash = { "beautysh" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        cs = { "clang-format" },
+        go = { "gofumpt" },
+        lua = { "stylua" },
+        python = { "autopep8" },
+        rust = {"rustfmt"}, -- rustup component add rustfmt-latest
 				["_"] = { "prettier" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
 			},
-			-- format_on_save = {
-			-- 	timeout_ms = 500,
-			-- 	lsp_fallback = true,
-			-- },
 		})
 		vim.keymap.set("n", "<leader>gf", function()
 			require("conform").format({ async = true, lsp_fallback = true })
