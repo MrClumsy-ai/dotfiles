3f3fd0b8406f9d787e174de97629062f6b8ef0af
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-08 02:24:07 -0600

    fix(nvim/formatting): prettier formatting all non-specified files

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,26 +1,27 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "pylsp" },
-        go = { "gofumpt" },
-        c = { "ast-grep" },
-        bash = { "beautysh" },
-        ["*"] = { "prettier" },
-      },
-      default_format_opts = {
-        lsp_format = "fallback",
-      },
-      -- format_on_save = {
-      -- 	timeout_ms = 500,
-      -- 	lsp_fallback = true,
-      -- },
-    })
-    vim.keymap.set('n', '<leader>gf', function()
-      require('conform').format({ async = true, lsp_fallback = true })
-    end, { desc = 'Format file with conform.nvim' })
-  end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "autopep8" },
+				go = { "gofumpt" },
+				c = { "clang-format" },
+				bash = { "beautysh" },
+				cs = { "clang-format" },
+				["_"] = { "prettier" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			-- format_on_save = {
+			-- 	timeout_ms = 500,
+			-- 	lsp_fallback = true,
+			-- },
+		})
+		vim.keymap.set("n", "<leader>gf", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, { desc = "Format file with conform.nvim" })
+	end,
 }
