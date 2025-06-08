29153bca8d0478eb73b0d7e117f10b1c498c2cac
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-08 13:51:14 -0600

    mod(nvim): refactored for git

--- nvim/.config/nvim/lua/plugins/utilities.lua
+++ nvim/.config/nvim/lua/plugins/utilities.lua
@@ -56,26 +56,6 @@ return {
 		end,
 	},
 	{
-		"tpope/vim-fugitive",
-		config = function()
-			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-			vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
-			vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>")
-		end,
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "-" },
-				topdelete = { text = "_" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-	{
 		"laytan/cloak.nvim",
 		config = function()
 			require("cloak").setup({
