a964b809efc9ce864c574b12c2b97dc37a06ce28
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-14 06:41:42 -0600

    mod(nvim/formatting): c# added

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,21 +1,30 @@
 return {
 	"stevearc/conform.nvim",
+	dependencies = {
+		"Hoffs/omnisharp-extended-lsp.nvim",
+	},
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-        bash = { "beautysh" },
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        cs = { "clang-format" },
-        go = { "gofumpt" },
-        lua = { "stylua" },
-        python = { "autopep8" },
-        rust = {"rustfmt"}, -- rustup component add rustfmt-latest
+				bash = { "beautysh" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				cs = { "clang-format" },
+				go = { "gofumpt" },
+				lua = { "stylua" },
+				python = { "autopep8" },
+				rust = { "rustfmt" }, -- rustup component add rustfmt-latest
 				["_"] = { "prettier" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
+			},
+			formatters = {
+				csharpier = {
+					command = "dotnet-csharpier",
+					args = { "--write-stdout" },
+				},
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", function()
