6501df57659787500fad3d8ae99f54a2b665d743
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-14 06:41:02 -0600

    mod(nvim/lsp): added descriptions to remaps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -26,15 +26,15 @@ return {
 				end,
 			},
 		})
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
-		vim.keymap.set("n", "<leader>vd", function ()
-      vim.diagnostic.open_float()
-		end)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP rename" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP get definition" })
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "LSP get implementation" })
+		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "LSP get references" })
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
+		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "LSP signature help" })
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, { desc = "LSP view diagnostics" })
 	end,
 }
