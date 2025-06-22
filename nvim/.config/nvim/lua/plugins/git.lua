255d708541c4c641100e5d659a2e0767ce5ff643
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-22 13:55:50 -0600

    mod(nvim/git): ctrl + k to accept suggestion

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -22,7 +22,7 @@ return {
 	{
 		"github/copilot.vim",
 		config = function()
-			vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+			vim.keymap.set("i", "<C-K>", 'copilot#Accept("\\<CR>")', {
 				expr = true,
 				replace_keycodes = false,
 			})
