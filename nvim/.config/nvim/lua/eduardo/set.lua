e4fd44cebfed6203bf0c3deeb0278b9814e9e2ff
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-07-29 13:45:37 -0600

    mod(nvim/set): scrolloff set to 10

--- nvim/.config/nvim/lua/eduardo/set.lua
+++ nvim/.config/nvim/lua/eduardo/set.lua
@@ -19,8 +19,8 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
